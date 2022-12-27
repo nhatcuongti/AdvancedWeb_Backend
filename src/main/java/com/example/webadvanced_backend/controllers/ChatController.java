0ec9317ca437766c2ef47e049c5cb022f1bbded9
lhnhat <nhatks147@gmail.com>
2022-12-27 21:15:54 +0700

    adjust code

--- src/main/java/com/example/webadvanced_backend/controllers/ChatController.java
+++ src/main/java/com/example/webadvanced_backend/controllers/ChatController.java
@@ -1,29 +1,31 @@
 package com.example.webadvanced_backend.controllers;
 
+import com.example.webadvanced_backend.models.Message;
 import com.example.webadvanced_backend.requestentities.SendMessageRequest;
-import org.eclipse.aether.resolution.ResolutionErrorPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping(path ="api/v1/message")
+@RequestMapping(path ="api/v1/chat")
 @CrossOrigin
-public class MessageController {
+public class ChatController {
     @Autowired
     SimpMessagingTemplate simpMessagingTemplate;
 
     @PostMapping(path = "/send-message/{preSessionId}")
      public ResponseEntity<?> sendMessage(@PathVariable String preSessionId, @RequestBody
-            SendMessageRequest request){
+            SendMessageRequest request, Principal principal){
         try{
             // 1 get message and user
+            Message message = Message.builder().username(principal.getName()).message(request.getMessage()).build();
             // 2 save message into temporary memory
             // 3 send to socket
-            simpMessagingTemplate.convertAndSend("/topic/chatroom/" + preSessionId);
+            simpMessagingTemplate.convertAndSend("/topic/chatroom/" + preSessionId, message);
             return ResponseEntity.ok("success to send message");
         }
         catch (Exception e){
---
+++ src/main/java/com/example/webadvanced_backend/models/Message.java
@@ -0,0 +1,15 @@
+package com.example.webadvanced_backend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+    private String username;
+    private String message;
+}
